ab36af74bb42199bfcc1f73be158339b8b1c6733
Cade Wilson <68790010+cade25wilson@users.noreply.github.com>
2020-08-15 19:33:24 -0600

    JavaScript Folder
    added basic JavaScript assignments

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+window.alert("Hello, World!"); //Alert box with text
+
+document.write("This is the Javascript course");    //Telling document to write that text
+
+document.write("<br>\"Happiness is not a goal"
++" it is a by-product\""
++ "-Elanor Roosevelt<br>");   //Concatenated string making one text
+
+var B=" Part one" + " and" + " part two"
+document.write(B)   //Assigning variable "B" a value and writing it out
+
+var Dealership = "Dodge", Suv="Duragno", Sedan="Charger", Coupe="Challenger", compact="Dart";
+var Suv =Suv.fontcolor("red");
+var Sedan=Sedan.fontcolor("green");
+var Coupe=Coupe.fontcolor("Orange");
+var compact=compact.fontcolor("Yellow");
+document.write(Sedan);      //assigning values to variables followed by assignining font color to variables and telling document to write variable in the givin fontcolor
+
+document.write (4 + 25); //"creating expression to be written"
