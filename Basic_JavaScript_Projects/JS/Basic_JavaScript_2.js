a0b0195357a60c8cb2a8be43ca883d3d401d161d
Cade Wilson <68790010+cade25wilson@users.noreply.github.com>
2020-08-16 05:13:21 -0600

    Basic_JavaScript_2
    added JavaScript needed to run Basic_HTML_2.

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str = "Welcome to my webpage!";
+    var result = str.fontsize("40px");
+    document.getElementById("Big_font").innerHTML=result; 
+}
+
+function myFunction() {
+    var sentence = "Functions can be hard,";
+    sentence += " but they are very important to learn.";
+    document.getElementById("string").innerHTML=sentence;   
+}
