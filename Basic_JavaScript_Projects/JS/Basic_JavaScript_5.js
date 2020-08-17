57e20414d376949b65ec8950467e23542e79aab3
Cade Wilson <68790010+cade25wilson@users.noreply.github.com>
2020-08-17 01:03:00 -0600

    Basic_JavaScript_5.JavaScript
    added JavaScript file needed for basic_html_5

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,59 @@
+document.write(typeof 3);   
+
+function true_Function() {
+    document.getElementById("String").innerHTML= isNaN ("this is not a number");
+}
+
+function false_Function() {
+    document.getElementById("Number").innerHTML= isNaN("652");
+}
+
+document.write(2E433);
+
+document.write(-2E422);
+
+document.write(22<25);
+
+document.write(122>1222)
+
+console.log(5+5);
+
+document.write("104"+22);
+
+console.log(22<14);
+
+document.write(22==22);
+
+document.write(13==2);
+
+g=20;
+k=20;
+document.write(g === k);
+
+s=15;
+r=12;
+document.write(s === r);
+
+c="cheese"
+d=24
+document.write(c === d);
+
+e="ten"
+f=10
+document.write (e === f);
+
+document.write(3>1 && 6>5);
+
+document.write(3>2 && 6<5);
+
+document.write(42>6 || 12<3);
+
+document.write(22<4 || 3>29)
+
+function not_Function() {
+    document.getElementById("Not").innerHTML= !(502 > 10);
+}
+
+function yes_Function() {
+    document.getElementById("yes").innerHTML = !(66 > 189);
+}
